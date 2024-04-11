f67f70de42d9a1035d796e201703d2dd46a73c53
Yevhen Herasymenko <herasymenko.yevhen@gmail.com>
2024-04-11 13:24:26 +0300

    scroll view

--- TestReaddle/TestReaddle/ContentView.swift
+++ TestReaddle/TestReaddle/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            contentView
         }
-        .padding()
+//        .padding()
+    }
+
+    @ViewBuilder
+    var contentView: some View {
+        Grid(alignment: .center, verticalSpacing: 10) {
+            GridRow {
+                rowContent
+            }
+        }
+        .background(.gray)
+
+    }
+
+    @ViewBuilder
+    var rowContent: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 20) {
+                ForEach(0..<10) { _ in
+                    Color.red
+                        .frame(width: 100, height: 100)
+                }
+            }
+        }
+        .frame(height: 150)
     }
 }
 

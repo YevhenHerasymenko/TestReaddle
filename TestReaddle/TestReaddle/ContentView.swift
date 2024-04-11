9572ffcbb49cceaaca24a6be3300c9c183d66da5
Yevhen Herasymenko <herasymenko.yevhen@gmail.com>
2024-04-11 13:04:11 +0300

    [Ticketless] init project

---
+++ TestReaddle/TestReaddle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestReaddle
+//
+//  Created by Yevhen Herasymenko on 11.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestReaddle/TestReaddle/TestReaddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReaddleApp.swift
+//  TestReaddle
+//
+//  Created by Yevhen Herasymenko on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestReaddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
